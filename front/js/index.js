82bc810a42dae7ca91ed5970adddd57a82cf3b8b
LaureZammit <zammitlaure@gmail.com>
2023-10-03 11:40:23 +0200

    Initialisation du repo

---
+++ front/js/index.js
@@ -0,0 +1,37 @@
+/* 
+Afficher pour chaque article en vente, les données depuis la base de données : 
+    <article>
+        <img src="img/01.png" alt="Titre produit">
+        <a href="product.html">Buy bird</a>
+    </article> 
+*/
+
+// http://localhost:3000/api/products/
+
+let datas = []
+
+async function init() {
+    datas = await result()
+    console.log(datas)
+    showDatas(datas, 1)
+}
+
+init()
+
+async function result() {
+    const requete = await fetch('http://localhost:3000/api/products/')
+    return await requete.json()
+}
+
+function showDatas(datas, id) {
+    for (let i = 0; i < datas.length; i++) {
+        document.querySelector(".products").innerHTML += `
+            <article>
+                <img src="${datas[i].image}" alt="${datas[i].name}">
+                <a href="product.html?id=${datas[i]._id}">
+                    Buy ${datas[i].shorttitle}
+                </a>
+            </article>
+        `;
+    }
+}
