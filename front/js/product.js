d03af59a5c86cdcac11ee44977f3dbc46798d0c8
LaureZammit <zammitlaure@gmail.com>
2023-10-04 14:30:25 +0200

    Fonctionnement localStorage >> >> Ajout des articles au localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -13,7 +13,6 @@ async function init() {
     datas = await resultIdOpen()
     // console.log(datas)
     showDatas(datas, 1)
-    buyArticles(datas, 1)
 }
 
 init()
@@ -65,25 +64,78 @@ function showDatas(datas, id) {
     document.querySelector(".description").innerHTML = datas.description
 }
 
+const quantity = document.querySelector(".quantity")
 
-buttonBuy.addEventListener('click', () => {
-    let optionsProduct = 
-    {
-        productId : productId,
-        quantity : document.getElementById('quantity').ariaValueMax,
-        format : document.getElementById('format').value
+// quantity.addEventListener('change', function () {
+//     let valeur = parseInt(quantity.value)
+
+//     if (valeur < 0 || valeur > 100) {
+//         valeur = 0;
+//         quantity.value = valeur
+//     }
+// })
+
+const buttonBuy = document.querySelector(".button-buy")
+
+buttonBuy.addEventListener('click', (e) => {
+    e.preventDefault()
+    const quantite = document.querySelector("input").value
+    const format = document.querySelector('#format').value;
+    const produit = {
+        id: datas._id,
+        titre: datas.titre,
+        image: datas.image,
+        quantite:quantite,
+        format: format
+    };
+
+    ajouterAuPanier(produit, quantite, format);
+
+    let panier = localStorage.getItem("panier");
+    if (panier === null || panier < 0) {
+        panier = [];
+    } else {
+        panier = JSON.parse(panier);
     }
-    let productInLocalStorage = JSON.parse(localStorage.getItem("product"))
 
-    if(productInLocalStorage) {
-        productInLocalStorage.push(optionsProduct)
-        localStorage.setItem("product", JSON.stringify(productInLocalStorage))
+    localStorage.setItem("panier", JSON.stringify(panier));
+    
+    document.querySelector(".modal").innerHTML = `
+    <div class="modal-content">
+    <p>Produit ajouté au panier !</p>
+    <a href="cart.html">Voir le panier</a>
+    <button class="close-modal">Fermer</button>
+    </div>
+    `;
+    document.querySelector(".modal").style.display = "flex";
+    
+    const modal = document.querySelector(".close-modal");
+    modal.addEventListener("click", () => {
+        document.querySelector(".modal").style.display = "none";
+    });
+});
+
+function ajouterAuPanier(produit, quantite, format) {
+
+    let panier = localStorage.getItem("panier");
+    panier = panier ? JSON.parse(panier) : [];
+
+    const produitExistant = panier.find(item => item.id === produit.id && item.format === format);
+
+    if (produitExistant) {
+        produitExistant.quantite += parseInt(quantite);
+        if(produitExistant.quantite >= 100) {
+            // On ne peut pas dépasser 100 de quantité par produit et par format
+            
+        }
+    } else {
+        panier.push({ id: produit.id, quantite: parseInt(quantite), format: format, produit });
     }
-})
 
+    localStorage.setItem("panier", JSON.stringify(panier));
+}
 
-
-// localStorag.setItem("key", "value")
+// localStorage.setItem("key", "value")
 
 // <!-- Afficher le contenu d'un localStorage -->
 // const key = localStorage.getItem("key")
