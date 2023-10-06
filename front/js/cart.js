7b48bc5a1fa41e14190bfb15e78c427ba1c77197
LaureZammit <zammitlaure@gmail.com>
2023-10-06 15:59:13 +0200

    Dernieres modifications

--- front/js/cart.js
+++ front/js/cart.js
@@ -4,9 +4,12 @@ let datasPanier;
 
 function afficherPanier() {
     let sommePrix = 0
+    if(panierJson.length === 0) {
+        document.querySelector("#row-cart").innerHTML = "<p>Votre panier est vide. Veuillez ajouter au moins un article à votre panier.</p>";
+    }
     
     if(panierJson.length > 0) {
-        panierJson.forEach(async (el) => {
+        panierJson.forEach(async el => {
             const recup = await fetch(`http://localhost:3000/api/products/${el.produit.id}`)
             datasPanier = await recup.json()
         
@@ -15,17 +18,17 @@ function afficherPanier() {
             const indexOptions = datasPanier.declinaisons.findIndex(item => item.taille === el.format);
         
             rowCart.insertAdjacentHTML("afterbegin", `
-            <div class="flex-cart">
-                <img src=${el.produit.image} alt=${el.produit.shorttitle} \>
-                <h2 class="cart-title">${el.produit.titre}</h2>
-                <p class="format-cart">Format ${el.format}</p>
-                <p class="prix-cart">${datasPanier.declinaisons[indexOptions].prix}€</p>
-                <div>
-                    <p class="quantite-cart">Quantité : </p>
-                    <input class="quantite-input" type="number" name="quantity" value="${el.quantite}" minlength="1">
+                <div class="flex-cart">
+                    <img src=${el.produit.image} alt=${el.produit.shorttitle} \>
+                    <h2 class="cart-title">${el.produit.titre}</h2>
+                    <p class="format-cart">Format ${el.format}</p>
+                    <p class="prix-cart">${datasPanier.declinaisons[indexOptions].prix}€</p>
+                    <div>
+                        <p class="quantite-cart">Quantité : </p>
+                        <input class="quantite-input" type="number" name="quantity" value="${el.quantite}" minlength="1">
+                    </div>
+                    <a class="delete-article" href="#">Supprimer</a>
                 </div>
-                <a class="delete-article" href="#">Supprimer</a>
-            </div>
             `)
             sommePrix += el.quantite * datasPanier.declinaisons[0].prix
             const sommePrixFormatted = sommePrix.toFixed(2)
